0921a21212449f3561f560f9328de05b906f0577
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-02 11:29:01 -0300

    Introducing Interfaces

--- index.ts
+++ index.ts
@@ -1,13 +1,17 @@
-const post = {
+interface Post {
+  title: string;
+  daysOld: number;
+  published: boolean;
+}
+
+const post: Post = {
   title: "Latest TypeScript News",
   daysOld: 0,
   published: true,
 };
 
-const printPost = (postToPrint: {
-  title: string;
-  daysOld: number;
-  published: boolean;
-}) => {
+const printPost = (postToPrint: Post): string => {
   return `${postToPrint.title} (${postToPrint.daysOld} days old).`;
 };
+
+printPost(post);
