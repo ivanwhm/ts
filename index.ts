0149d1605719e0a5ba6f50479eb5af41f2fe368a
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-02 11:46:57 -0300

    Public and Private

--- index.ts
+++ index.ts
@@ -1,18 +1,26 @@
 class Car {
-  color: string;
-  year: number;
+  public color: string;
+  private year: number;
 
   constructor(color: string, year: number) {
     this.color = color;
     this.year = year;
   }
 
-  drive() {
-    console.log("Vroom");
+  public drive() {
+    this.putInGear();
+    this.pressPedal();
+    this.turnWheel();
   }
+
+  private putInGear() {}
+
+  private pressPedal() {}
+
+  private turnWheel() {}
 }
 
 const myCar = new Car("Red", 2012);
 myCar.drive();
 
-console.log(myCar.color, myCar.year);
+console.log(myCar.color);
