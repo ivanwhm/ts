f04b59226f3b6c525147ea5e31dcfdb676961c36
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-02 09:42:46 -0300

    Type Inference

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-const myName: string = "Ivan Wilhelm";
-const ten: number = 10;
-const negative: number = -10000;
-const decimal: number = 10.000001;
-const yes: boolean = true;
-const nothingHere: null = null;
-const noDefinition: undefined = undefined;
+let myName: string;
+
+if (1 === 1) {
+  myName = "Ivan";
+} else {
+  myName = "José";
+}
